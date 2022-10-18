3ec3c49845e982190347a8fb5fcfa90326862cb4
colinjfagan@gmail.com <colinjfagan@gmail.com>
2022-10-18 08:47:12 -0700

    GEA-258: Move to run only on main

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ PACKAGE_NAME=$(jq .name package.json)
 PACKAGE_VERSION=$(jq .version package.json)
 LATEST_PACKAGE_VERSION=$(npm show $PACKAGE_NAME version)
 
-if [ $PACKAGE_VERSION != $LATEST_PACKAGE_VERSION ]  ; then
+if [ "$PACKAGE_VERSION" != "$LATEST_PACKAGE_VERSION" ]  ; then
     yarn build
 	yarn publish --new-version $PACKAGE_VERSION
 else
