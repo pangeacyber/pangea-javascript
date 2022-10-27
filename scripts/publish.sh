8c81a15d0323cd751961a7a8cf5773c370b5043c
colinjfagan@gmail.com <colinjfagan@gmail.com>
2022-10-26 19:06:32 -0700

    GEA-2073: Update to fix pipeline

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 PACKAGE_NAME=$(jq .name package.json)
+echo "Looking to update: $PACKAGE_NAME"
+if [ $PACKAGE_NAME = "" ] ; then
+    exit 1
+fi
 
 PACKAGE_VERSION=$(jq .version package.json)
+echo "Current package version: $PACKAGE_VERSION"
+
 LATEST_PACKAGE_VERSION=$(npm show $PACKAGE_NAME version)
+echo "Published packaged version $LATEST_PACKAGE_VERSION"
 
 if [ $PACKAGE_VERSION != $LATEST_PACKAGE_VERSION ] ; then
     yarn build
