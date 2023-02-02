e43042ed6ae3e88dcb25741fbc29eed612f686d9
David Wayman <david.wayman@pangea.cloud>
2023-02-02 15:12:01 -0800

    GEA-4205: update redact doc strings

--- packages/pangea-node-sdk/src/services/redact.ts
+++ packages/pangea-node-sdk/src/services/redact.ts
@@ -15,8 +15,12 @@ class RedactService extends BaseService {
 
   /**
    * @summary Redact
-   * @description Redact sensitive information from provided text.
-   * @param {string} text - The text data to redact.
+   * @description Redact sensitive information from provided text
+   * @param {String} text - The text data to redact
+   * @param {Object} options - Supported options:
+   *   - debug {Boolean} - Setting this value to true will provide a detailed analysis of the redacted
+   * data and the rules that caused redaction
+   *   - rules {String[]} - An array of redact rule short names
    * @returns {Promise} - A promise representing an async call to the redact endpoint
    * @example
    * const response = await redact.redact("Jenny Jenny... 415-867-5309");
@@ -37,8 +41,15 @@ class RedactService extends BaseService {
 
   /**
    * @summary Redact structured
-   * @description Redact sensitive information from structured data (e.g., JSON).
-   * @param {Object} text - Structured data to redact
+   * @description Redact sensitive information from structured data (e.g., JSON)
+   * @param {Object} data - Structured data to redact
+   * @param {Object} options - Supported options:
+   *   - debug {Boolean} - Setting this value to true will provide a detailed analysis of the redacted
+   * data and the rules that caused redaction
+   *   - rules {String[]} - An array of redact rule short names
+   *   - jsonp {String[]} - JSON path(s) used to identify the specific JSON fields to redact in the
+   * structured data. Note: If jsonp parameter is used, the data parameter must be in JSON format.
+   *   - format {String} - The format of the structured data to redact. Default: "json"
    * @returns {Promise} - A promise representing an async call to the redactStructured endpoint
    * @example
    * const data = { "phone": "415-867-5309" };
