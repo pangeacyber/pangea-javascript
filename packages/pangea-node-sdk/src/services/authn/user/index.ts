5ce0145f134d0e8be39c8b4d3a47984c3f1d47a9
David Wayman <david.wayman@pangea.cloud>
2023-04-27 16:57:50 -0700

    Add doc strings for authn user

--- packages/pangea-node-sdk/src/services/authn/user/index.ts
+++ packages/pangea-node-sdk/src/services/authn/user/index.ts
@@ -25,12 +25,18 @@ export default class User extends BaseService {
 
   // authn::/v1/user/delete
   /**
-   * @summary Delete a user
-   * @description Delete a user
-   * @param {Request} request - An email/id delete request
-   * @returns {Promise<PangeaResponse<{}>>} - A promise representing an async call to the endpoint
+   * @summary Delete User
+   * @description Delete a user.
+   * @operationId authn_post_v1_user_delete
+   * @param {Object} request - Supported options:
+   *   - email (string): An email address
+   *   - id (string): The identity of a user or a service
+   * @returns {Promise<PangeaResponse<{}>>} - A promise
+   * representing an async call to the endpoint.
    * @example
-   * await authn.user.delete({email: "example@example.com"});
+   * await authn.user.delete(
+   *   { email: "example@example.com" }
+   * );
    */
   delete(
     request: AuthN.User.Delete.EmailRequest | AuthN.User.Delete.IDRequest
@@ -39,6 +45,41 @@ export default class User extends BaseService {
   }
 
   // authn::/v1/user/create
+  /**
+   * @summary Create User
+   * @description Create a user.
+   * @operationId authn_post_v1_user_create
+   * @param {String} email - An email address
+   * @param {String} authenticator - A provider-specific authenticator,
+   * such as a password or a social identity.
+   * @param {AuthN.IDProvider} idProvider - Mechanism for authenticating a
+   * user's identity
+   * @param {Object} options - Supported options:
+   *   - verified (boolean):  True if the user's email has been verified
+   *   - require_mfa (boolean): True if the user must use MFA
+   * during authentication
+   *   - profile (object): A user profile as a collection of string properties
+   *   - scopes (string[]): A list of scopes
+   * @returns {Promise<PangeaResponse<AuthN.User.CreateResult>>} - A promise
+   * representing an async call to the endpoint.
+   * @example
+   * ```js
+   * const response = await authn.user.create(
+   *   "joe.user@email.com",
+   *   "My1s+Password",
+   *   AuthN.IDProvider.PASSWORD,
+   *   {
+   *     verified: false,
+   *     require_mfa: false,
+   *     profile: {
+   *       first_name: "Joe",
+   *       last_name: "User",
+   *     }
+   *     scopes: ["scope1", "scope2"],
+   *   }
+   * );
+   * ```
+   */
   create(
     email: string,
     authenticator: string,
@@ -60,6 +101,29 @@ export default class User extends BaseService {
   }
 
   // authn::/v1/user/invite
+  /**
+   * @summary Invite User
+   * @description Send an invitation to a user.
+   * @operationId authn_post_v1_user_invite
+   * @param {String} inviter - An email address
+   * @param {String} email - An email address
+   * @param {String} callback - A login callback URI
+   * @param {String} state - State tracking string fo login callbacks
+   * @param {Object} options - Supported options:
+   *   - require_mfa (boolean): Require the user to authenticate with MFA
+   * @returns {Promise<PangeaResponse<AuthN.User.InviteResult>>} - A promise
+   * representing an async call to the endpoint.
+   * @example
+   * ```js
+   * const response = await authn.user.invite(
+   *   "admin@email.com",
+   *   "joe.user@email.com",
+   *   "https://www.myserver.com/callback",
+   *   "pcb_zurr3lkcwdp5keq73htsfpcii5k4zgm7",
+   *   { require_mfa: false }
+   * );
+   * ```
+   */
   invite(
     inviter: string,
     email: string,
@@ -78,12 +142,56 @@ export default class User extends BaseService {
   }
 
   // authn::/v1/user/list
+  /**
+   * @summary List Users
+   * @description Look up users by scopes.
+   * @operationId authn_post_v1_user_list
+   * @param {Object} options - Supported options:
+   *   - filter (object)
+   *   - last (string): Reflected value from a previous response to
+   * obtain the next page of results.
+   *   - order (AuthN.ItemOrder): Order results asc(ending) or desc(ending).
+   *   - order_by (AuthN.User.ListOrderBy): Which field to order results by.
+   *   - size (number): Maximum results to include in the response.
+   * @returns {Promise<PangeaResponse<AuthN.User.ListResult>>} - A promise
+   * representing an async call to the endpoint.
+   * @example
+   * ```js
+   * const response = await authn.user.list(
+   *   {
+   *     order: AuthN.ItemOrder.ASC,
+   *     order_by: AuthN.User.ListOrderBy.ID,
+   *     size: 20,
+   *   }
+   * );
+   * ```
+   */
   list(request: AuthN.User.ListRequest): Promise<PangeaResponse<AuthN.User.ListResult>> {
     request.use_new = true;
     return this.post("user/list", request);
   }
 
   // authn::/v1/user/verify
+  /**
+   * @summary Verify User
+   * @description Verify a user's primary authentication.
+   * @operationId authn_post_v1_user_verify
+   * @param {AuthN.IDProvider} idProvider - Mechanism for authenticating a
+   * user's identity
+   * @param {String} email - An email address
+   * @param {String} authenticator - A provider-specific authenticator,
+   * such as a password or a social identity.
+   * @returns {Promise<PangeaResponse<AuthN.User.VerifyResult>>} - A promise
+   * representing an async call to the endpoint.
+   * @example
+   * ```js
+   * const response = await authn.user.verify(
+   *   AuthN.IDProvider.PASSWORD,
+   *   "joe.user@email.com",
+   *   "My1s+Password"
+   * );
+   * ```
+   */
   verify(
     idProvider: AuthN.IDProvider,
     email: string,
@@ -98,6 +206,32 @@ export default class User extends BaseService {
   }
 
   // authn::/v1/user/update
+  /**
+   * @summary Update user's settings
+   * @description Update user's settings.
+   * @operationId authn_post_v1_user_update
+   * @param {Object} request - Supported request:
+   *   - email (string): An email address
+   *   - id (string): The identity of a user or a service
+   * @param {Object} options - Supported options:
+   *   - authenticator (string): New value for a user's authenticator.
+   *   - disabled (boolean): New disabled value.
+   * Disabling a user account will prevent them from logging in.
+   *   - require_mfa (boolean): New require_mfa value
+   *   - verified (boolean): New verified value
+   * @returns {Promise<PangeaResponse<AuthN.User.UpdateResult>>} - A promise
+   * representing an async call to the endpoint.
+   * @example
+   * ```js
+   * const response = await authn.user.update(
+   *   { email: "joe.user@email.com" },
+   *   {
+   *     disabled: false,
+   *     require_mfa: true,
+   *   }
+   * );
+   * ```
+   */
   update(
     request: AuthN.User.Update.EmailRequest | AuthN.User.Update.IDRequest,
     options: AuthN.User.Update.Options
