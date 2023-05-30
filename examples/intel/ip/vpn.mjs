8e4d1ad701c21cd25905561394b54ff8c68ada2f
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-05-30 18:08:48 -0300

    intel ip examples

---
+++ examples/intel/ip/vpn.mjs
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+import { PangeaConfig, IPIntelService, PangeaErrors } from "pangea-node-sdk";
+
+const domain = process.env.PANGEA_DOMAIN;
+const token = process.env.PANGEA_INTEL_TOKEN;
+const config = new PangeaConfig({ domain: domain });
+const ipIntel = new IPIntelService(String(token), config);
+
+(async () => {
+  console.log("Checking IP is a VPN...");
+
+  const options = { provider: "digitalelement", verbose: true, raw: true };
+  try {
+    const response = await ipIntel.isVPN("2.56.189.74", options);
+    if(response.result.data.is_vpn === true){
+      console.log("IP is a VPN");
+    } else {
+      console.log("IP is not a VPN");
+    }
+  } catch (e) {
+    if (e instanceof PangeaErrors.APIError) {
+      console.log("Error", e.summary, e.errors);
+    } else {
+      console.log("Error: ", e);
+    }
+  }
+})();
