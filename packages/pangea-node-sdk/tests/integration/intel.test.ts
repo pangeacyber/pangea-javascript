ac7a6e13136e6e3fe452d95d6c11c9670319f774
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-08-08 10:56:55 -0300

    update to user crowdstrike

--- packages/pangea-node-sdk/tests/integration/intel.test.ts
+++ packages/pangea-node-sdk/tests/integration/intel.test.ts
@@ -266,7 +266,7 @@ it("User password breached with default provider should succeed", async () => {
 jest.setTimeout(60000);
 it("File Scan ", async () => {
   try {
-    const request = { verbose: true, raw: true, provider: "reversinglabs" };
+    const request = { verbose: true, raw: true, provider: "crowdstrike" };
     const response = await fileScan.fileScan(request, eicarFilePath);
 
     expect(response.status).toBe("Success");
@@ -281,7 +281,7 @@ it("File Scan ", async () => {
 jest.setTimeout(60000);
 it("File Scan async ", async () => {
   try {
-    const request = { verbose: true, raw: true, provider: "reversinglabs" };
+    const request = { verbose: true, raw: true, provider: "crowdstrike" };
     const response = await fileScan.fileScan(request, eicarFilePath, { pollResultSync: false });
     expect(false).toBeTruthy();
   } catch (e) {
@@ -305,7 +305,7 @@ jest.setTimeout(120000);
 it("File Scan async and poll result", async () => {
   let exception;
   try {
-    const request = { verbose: true, raw: true, provider: "reversinglabs" };
+    const request = { verbose: true, raw: true, provider: "crowdstrike" };
     const response = await fileScan.fileScan(request, eicarFilePath, { pollResultSync: false });
     expect(false).toBeTruthy();
   } catch (e) {
