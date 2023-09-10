d8f2fbf3f43dff86b2ab4ea59b9840e7f265fb44
colinjfagan@gmail.com <colinjfagan@gmail.com>
2023-09-10 08:29:24 -0700

    GEA-9108

--- packages/pangea-node-sdk/src/config.ts
+++ packages/pangea-node-sdk/src/config.ts
@@ -1,6 +1,6 @@
 import { ConfigOptions, ConfigEnv } from "./types.js";
 
-export const version = "2.2.0";
+export const version = "2.2.1";
 
 class PangeaConfig {
   domain: string = "pangea.cloud";
