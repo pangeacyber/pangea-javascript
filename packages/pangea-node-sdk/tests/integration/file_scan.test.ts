2a2b2ad03a95f708e9b110259a568ae46e5c4f97
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-09-13 16:43:44 -0300

    add reversinglabs examples

--- packages/pangea-node-sdk/tests/integration/file_scan.test.ts
+++ packages/pangea-node-sdk/tests/integration/file_scan.test.ts
@@ -13,7 +13,12 @@ const fileScan = new FileScanService(token, config);
 const testfilePath = "./tests/testdata/testfile.pdf";
 jest.setTimeout(60000);
 
-it("File Scan ", async () => {
+const delay = async (ms: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+
+it("File Scan crowdstrike", async () => {
   try {
     const request = { verbose: true, raw: true, provider: "crowdstrike" };
     const response = await fileScan.fileScan(request, testfilePath);
@@ -27,7 +32,7 @@ it("File Scan ", async () => {
   }
 });
 
-it("File Scan async ", async () => {
+it("File Scan crowdstrike async", async () => {
   try {
     const request = { verbose: true, raw: true, provider: "crowdstrike" };
     await fileScan.fileScan(request, testfilePath, { pollResultSync: false });
@@ -44,15 +49,68 @@ it("File Scan async ", async () => {
   }
 });
 
-const delay = async (ms: number) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-
-it("File Scan async and poll result", async () => {
+it("File Scan crowdstrike async and poll result", async () => {
   let exception;
   try {
     const request = { verbose: true, raw: true, provider: "crowdstrike" };
+    await fileScan.fileScan(request, testfilePath, { pollResultSync: false });
+    expect(false).toBeTruthy();
+  } catch (e) {
+    expect(e).toBeInstanceOf(PangeaErrors.APIError);
+    if (e instanceof PangeaErrors.AcceptedRequestException) {
+      expect(e.pangeaResponse.status).toBe("Accepted");
+      expect(e.errors.length).toBe(0);
+      exception = e;
+    } else {
+      console.log(e);
+      expect(false).toBeTruthy();
+    }
+  }
+
+  // Wait until result could be ready
+  await delay(30 * 1000);
+  const request_id = exception?.request_id || "";
+  const response = await fileScan.pollResult(request_id);
+  expect(response.status).toBe("Success");
+  expect(response.result.data).toBeDefined();
+  expect(response.result.data.verdict).toBe("benign");
+});
+
+it("File Scan reversinglabs", async () => {
+  try {
+    const request = { verbose: true, raw: true, provider: "reversinglabs" };
+    const response = await fileScan.fileScan(request, testfilePath);
+
+    expect(response.status).toBe("Success");
+    expect(response.result.data).toBeDefined();
+    expect(response.result.data.verdict).toBe("benign");
+  } catch (e) {
+    console.log(e);
+    expect(false).toBeTruthy();
+  }
+});
+
+it("File Scan reversinglabs async", async () => {
+  try {
+    const request = { verbose: true, raw: true, provider: "reversinglabs" };
+    await fileScan.fileScan(request, testfilePath, { pollResultSync: false });
+    expect(false).toBeTruthy();
+  } catch (e) {
+    expect(e).toBeInstanceOf(PangeaErrors.APIError);
+    if (e instanceof PangeaErrors.AcceptedRequestException) {
+      expect(e.pangeaResponse.status).toBe("Accepted");
+      expect(e.errors.length).toBe(0);
+    } else {
+      console.log(e);
+      expect(false).toBeTruthy();
+    }
+  }
+});
+
+it("File Scan reversinglabs async and poll result", async () => {
+  let exception;
+  try {
+    const request = { verbose: true, raw: true, provider: "reversinglabs" };
     await fileScan.fileScan(request, testfilePath, { pollResultSync: false });
     expect(false).toBeTruthy();
   } catch (e) {
