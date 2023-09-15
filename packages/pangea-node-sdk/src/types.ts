77ba2056d08370eae561816ee32ca4a00d64f43e
H. Andrés Tournour <111303288+pangea-andrest@users.noreply.github.com>
2023-09-15 13:46:52 -0300

    fix intel breached hash type support (#277)
    * fix intel breached hash type support

--- packages/pangea-node-sdk/src/types.ts
+++ packages/pangea-node-sdk/src/types.ts
@@ -223,7 +223,8 @@ export namespace Intel {
   export enum HashType {
     SHA256 = "sha256",
     SHA1 = "sha1",
-    MD5 = "md5",
+    SHA512 = "sha512",
+    NTLM = "ntlm",
   }
 
   export interface Options {
