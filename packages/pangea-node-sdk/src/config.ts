8b93566ee92351b482391475826113a25860fa08
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-09-26 11:00:07 -0300

    v2.2.2

--- packages/pangea-node-sdk/src/config.ts
+++ packages/pangea-node-sdk/src/config.ts
@@ -1,6 +1,6 @@
 import { ConfigOptions, ConfigEnv } from "./types.js";
 
-export const version = "2.2.1";
+export const version = "2.2.2";
 
 class PangeaConfig {
   domain: string = "pangea.cloud";
