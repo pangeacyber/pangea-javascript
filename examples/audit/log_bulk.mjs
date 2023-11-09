5d8316bcfb21c803e194b321caf5640b554b8287
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-11-09 15:11:24 -0300

    examples

---
+++ examples/audit/log_bulk.mjs
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+
+import { PangeaConfig, AuditService, PangeaErrors } from "pangea-node-sdk";
+
+const token = process.env.PANGEA_AUDIT_TOKEN;
+const config = new PangeaConfig({ domain: process.env.PANGEA_DOMAIN });
+const audit = new AuditService(token, config);
+
+(async () => {
+  const event1 = {
+    message: "Sign up",
+  };
+
+  const event2 = {
+    message: "Sign in",
+  };
+
+  try {
+    console.log("Logging multiple events");
+    const logResponse = await audit.logBulk([event1, event2], { verbose: true });
+    logResponse.result.results.forEach(result => {
+      console.log("Result: %s", result);      
+    });
+    
+  } catch (err) {
+    if (err instanceof PangeaErrors.APIError) {
+      console.log(err.summary, err.pangeaResponse);
+    } else {
+      throw err;
+    }
+  }
+})();
