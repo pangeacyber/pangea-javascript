2f5d57d92494ac5afa92ee50f969309d2ef69099
Govind Salinas <govind.salinas@pangea.cloud>
2023-12-19 12:43:23 -0800

    Camel case varaiables.

--- examples/audit/log_bulk_async_with_vault.mjs
+++ examples/audit/log_bulk_async_with_vault.mjs
@@ -7,9 +7,9 @@ import {
   VaultService,
 } from "pangea-node-sdk";
 
-const vault_token = process.env.PANGEA_VAULT_TOKEN;
+const vaultToken = process.env.PANGEA_VAULT_TOKEN;
 const config = new PangeaConfig({ domain: process.env.PANGEA_DOMAIN });
-const audit_token_vault_id = process.env.PANGEA_AUDIT_TOKEN_VAULT_ID;
+const auditTokenVaultId = process.env.PANGEA_AUDIT_TOKEN_VAULT_ID;
 
 (async () => {
   const event1 = {
@@ -18,12 +18,12 @@ const audit_token_vault_id = process.env.PANGEA_AUDIT_TOKEN_VAULT_ID;
 
   try {
     // Create the vault service instance
-    const vault = new VaultService(vault_token, config);
+    const vault = new VaultService(vaultToken, config);
     // Retrieve the audit token
-    const getLastResponse = await vault.getItem(audit_token_vault_id);
-    const audit_token = getLastResponse.result.current_version.secret;
+    const getLastResponse = await vault.getItem(auditTokenVaultId);
+    const auditToken = getLastResponse.result.current_version.secret;
     // Create the Audit instance and log events
-    const audit = new AuditService(audit_token, config);
+    const audit = new AuditService(auditToken, config);
     await audit.logBulkAsync([event1], { verbose: true });
     console.log("Success");
   } catch (err) {
