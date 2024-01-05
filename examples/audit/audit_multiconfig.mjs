14df2d340e4622a8321a297a65fa9971b3461c79
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-01-05 16:24:36 -0300

    audit and redact multiconfig example

---
+++ examples/audit/audit_multiconfig.mjs
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+
+import { PangeaConfig, AuditService, PangeaErrors } from "pangea-node-sdk";
+
+const token = process.env.PANGEA_AUDIT_MULTICONFIG_TOKEN;
+// const configId = process.env.PANGEA_AUDIT_CONFIG_ID;
+const configId = "";
+const config = new PangeaConfig({ domain: process.env.PANGEA_DOMAIN });
+
+// Set configId in service constructor
+const audit = new AuditService(token, config, undefined, configId);
+
+(async () => {
+  const data = {
+    message: "Hello, World!",
+  };
+
+  try {
+    console.log("Logging: %s", data.message);
+    const logResponse = await audit.log(data, { verbose: true });
+    console.log("Response: %s", logResponse.result);
+  } catch (err) {
+    if (err instanceof PangeaErrors.APIError) {
+      console.log(err.summary, err.toString());
+    } else {
+      throw err;
+    }
+  }
+})();
