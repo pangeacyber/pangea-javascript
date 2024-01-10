3bdda6e191970cc191691a443ef4d5930e4620a9
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-01-10 12:41:50 -0500

    Add structured encrypt example (GEA-11385)

--- examples/vault/README.md
+++ examples/vault/README.md
@@ -2,7 +2,7 @@
 
 ## Setup
 
-Set up environment variables ([Instructions](https://pangea.cloud/docs/getting-started/integrate/#set-environment-variables)) `PANGEA_VAULT_TOKEN` and `PANGEA_DOMAIN` with your project token configured on Pangea User Console (token should have access to Vault service [Instructions](https://pangea.cloud/docs/getting-started/configure-services/#configure-a-pangea-service)) and with your pangea domain.
+Set up environment variables ([Instructions](https://pangea.cloud/docs/getting-started/integrate/#set-environment-variables)) `PANGEA_VAULT_TOKEN` and `PANGEA_DOMAIN` with your project token configured on Pangea User Console (token should have access to Vault service [Instructions](https://pangea.cloud/docs/getting-started/configure-services/#configure-a-pangea-service)) and with your Pangea domain.
 
 ## Run example
 
---
+++ examples/vault/encrypt_structured.mjs
@@ -0,0 +1,37 @@
+/* eslint-disable no-console */
+
+import process from "node:process";
+
+import { PangeaConfig, VaultService, Vault } from "pangea-node-sdk";
+
+const token = process.env.PANGEA_VAULT_TOKEN;
+const config = new PangeaConfig({ domain: process.env.PANGEA_DOMAIN });
+const vault = new VaultService(token, config);
+
+(async () => {
+  // First create an encryption key, either from the Pangea Console or
+  // programmatically as below.
+  const createResponse = await vault.symmetricGenerate(
+    Vault.SymmetricAlgorithm.AES256_CFB,
+    Vault.KeyPurpose.ENCRYPTION,
+    "any unique name"
+  );
+  const encryptionKeyId = createResponse.result.id;
+
+  // Structured data that we'll encrypt.
+  const data = {
+    foo: [1, 2, "bar", "baz"],
+    some: "thing",
+  };
+
+  const response = await vault.encryptStructured({
+    id: encryptionKeyId,
+    structured_data: data,
+    filter: "$.foo[2:4]",
+  });
+
+  console.log(
+    "Encrypted result: %s",
+    JSON.stringify(response.result.structured_data, null, 2)
+  );
+})();
