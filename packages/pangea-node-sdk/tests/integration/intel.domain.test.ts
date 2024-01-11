03c23aa6564540c74772f9d488eb5e4d08d5b62f
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-01-11 13:54:36 -0300

    run intel tests against live

--- packages/pangea-node-sdk/tests/integration/intel.domain.test.ts
+++ packages/pangea-node-sdk/tests/integration/intel.domain.test.ts
@@ -3,7 +3,7 @@ import { jest, it, expect } from "@jest/globals";
 import { TestEnvironment, getTestDomain, getTestToken } from "../../src/utils/utils.js";
 import { DomainIntelService } from "../../src/index.js";
 
-const testEnvironment = TestEnvironment.DEVELOP;
+const testEnvironment = TestEnvironment.LIVE;
 
 const token = getTestToken(testEnvironment);
 const testHost = getTestDomain(testEnvironment);
