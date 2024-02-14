1194ba879fafe4629c03bf6dd898c342452294c1
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-02-14 12:30:37 -0500

    fixup! prettier

--- packages/react-mui-shared/src/components/RelativeDateRangeField/index.tsx
+++ packages/react-mui-shared/src/components/RelativeDateRangeField/index.tsx
@@ -146,9 +146,10 @@ export const getDisplayDateRange = (range: string): string => {
   const { amount, span, relativeRange } = getRelativeDateRange(range);
 
   const spanDisplay = get(RANGE_TO_DISPLAY, relativeRange, startCase(span));
-  const plural = !spanDisplay.endsWith("s") && typeof amount === "number" && amount > 1
-    ? "s"
-    : "";
+  const plural =
+    !spanDisplay.endsWith("s") && typeof amount === "number" && amount > 1
+      ? "s"
+      : "";
   return `${amount} ${spanDisplay}${plural}`;
 };
 
