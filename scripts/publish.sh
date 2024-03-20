0545881d73107c39aec7447e82006a4bb1d40715
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-03-20 16:13:58 -0400

    chore: support publishing to beta dist-tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
+set -e
+
 PACKAGE_NAME=$(jq .name package.json | tr -d '"')
 echo "Looking to update: $PACKAGE_NAME"
-if [ $PACKAGE_NAME = "" ] ; then
+if [ "$PACKAGE_NAME" = "" ] ; then
     exit 1
 fi
 
 PACKAGE_VERSION=$(jq .version package.json | tr -d '"')
 echo "Current package version: $PACKAGE_VERSION"
 
-LATEST_PACKAGE_VERSION=$(npm show $PACKAGE_NAME version)
+LATEST_PACKAGE_VERSION=$(npm show "$PACKAGE_NAME" version)
 echo "Published packaged version $LATEST_PACKAGE_VERSION"
 
 if [ "$PACKAGE_VERSION" != "$LATEST_PACKAGE_VERSION" ] ; then
-    yarn build
-	  yarn publish --new-version $PACKAGE_VERSION
+    echo yarn build
+
+    if [[ "$PACKAGE_VERSION" == *"beta"* ]] ; then
+	      echo yarn publish --new-version "$PACKAGE_VERSION" --tag beta
+    else
+        echo yarn publish --new-version "$PACKAGE_VERSION"
+    fi
 else
     echo "Package not updated. Skipping publish"
 fi
