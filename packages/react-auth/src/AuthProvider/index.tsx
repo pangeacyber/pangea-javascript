757f7f212519f11e61fe9506029a2d73c843bf1d
Glenn Gallien <glenn.gallien@pangea.cloud>
2024-04-22 15:23:12 -0700

    Revert "Pass `pm` param to hosted page."
    This reverts commit 91a313941e76c52a7cc4df8d09bab418ac42cd86.

--- packages/react-auth/src/AuthProvider/index.tsx
+++ packages/react-auth/src/AuthProvider/index.tsx
@@ -358,11 +358,6 @@ export const AuthProvider: FC<AuthProviderProps> = ({
       }
     }
 
-    // pass `pm` (product marketing) param
-    if (urlParams.has("pm")) {
-      query.append("pm", urlParams.get("pm"));
-    }
-
     const queryParams = query.toString();
     const redirectTo = loginURL;
     const url = queryParams ? `${redirectTo}?${queryParams}` : redirectTo;
