d9b722040b4e8c77c0fd9aa7994afff97cc21776
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-07-22 13:40:35 -0400

    examples: add Audit search example (PAN-15778)

---
+++ examples/audit/search.mjs
@@ -0,0 +1,20 @@
+// An example of searching Secure Audit Log for specific events.
+
+/* eslint-disable no-console */
+
+import { PangeaConfig, AuditService } from "pangea-node-sdk";
+
+const token = process.env.PANGEA_AUDIT_TOKEN;
+const config = new PangeaConfig({ domain: process.env.PANGEA_DOMAIN });
+const audit = new AuditService(token, config);
+
+(async () => {
+  // Search for audit events with actor "Dennis" or target "Grant". More
+  // information about the search syntax can be found at
+  // <https://pangea.cloud/docs/audit/using-secure-audit-log/searching-the-logs#search-syntax>.
+  const response = await audit.search('actor:"Dennis" OR target:"Grant"');
+
+  // Log the results.
+  console.log(`Found ${response.result.count} events.`);
+  console.log(response.result.events);
+})();
