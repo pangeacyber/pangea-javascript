3a87192689c1058f782a6aeff74354493a85acbd
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-10-20 19:23:50 -0400

    pangea-node-sdk: add Data Guard and Prompt Guard

--- packages/pangea-node-sdk/src/services/index.ts
+++ packages/pangea-node-sdk/src/services/index.ts
@@ -1,27 +1,31 @@
 import AuditService from "./audit.js";
 import AuthNService from "./authn/index.js";
 import AuthZService from "./authz.js";
-import EmbargoService from "./embargo.js";
 import BaseService from "./base.js";
-import RedactService from "./redact.js";
+import DataGuardService from "./data_guard.js";
+import EmbargoService from "./embargo.js";
+import { FileScanService } from "./file_scan.js";
 import {
-  FileIntelService,
   DomainIntelService,
+  FileIntelService,
   IPIntelService,
   URLIntelService,
   UserIntelService,
 } from "./intel.js";
-import VaultService from "./vault.js";
-import { FileScanService } from "./file_scan.js";
+import PromptGuardService from "./prompt_guard.js";
+import RedactService from "./redact.js";
 import SanitizeService from "./sanitize.js";
 import ShareService from "./share.js";
+import VaultService from "./vault.js";
 
 export default {
   AuditService,
   AuthNService,
   AuthZService,
+  DataGuardService,
   EmbargoService,
   BaseService,
+  PromptGuardService,
   RedactService,
   FileIntelService,
   DomainIntelService,
