3861b6698c7bb2156b03bfce95c404f9d62016b2
Glenn Gallien <glenn.gallien@pangea.cloud>
2024-12-02 10:25:59 -0800

    Add typedoc strings for vailla-js

--- packages/vanilla-js/src/AuthNFlowClient/types.ts
+++ packages/vanilla-js/src/AuthNFlowClient/types.ts
@@ -1,18 +1,21 @@
+/** Flow start options */
 export interface AuthNFlowOptions {
   signin?: boolean;
   signup?: boolean;
 }
 
+/** Callback parameters */
 export interface CallbackParams {
   code: string;
   state: string;
 }
 
-/*
+/**
   Flow data types
 */
 
 export namespace AuthFlow {
+  /** Flow endpoints */
   export enum Endpoint {
     START = "start",
     UPDATE = "update",
@@ -20,6 +23,7 @@ export namespace AuthFlow {
     RESTART = "restart",
   }
 
+  /** Flow choice options */
   export enum Choice {
     SET_EMAIL = "set_email",
     SET_USERNAME = "set_username",
@@ -44,12 +48,14 @@ export namespace AuthFlow {
     NONE = "",
   }
 
+  /** Username formats */
   export enum UsernameFormat {
     EMAIL = "email",
     STRING = "string",
     PHONE = "phone",
   }
 
+  /** Password policy definition */
   export interface PasswordPolicy {
     chars_min: number;
     chars_max: number;
@@ -59,12 +65,14 @@ export namespace AuthFlow {
     number_min: number;
   }
 
+  /** Flow start parameters */
   export interface StartParams {
     email?: string;
     device_id?: string;
     client_id?: string;
   }
 
+  /** Choice response */
   export type ChoiceResponse =
     | EmptyObject
     | PasswordResponse
@@ -78,6 +86,7 @@ export namespace AuthFlow {
     | TotpResponse
     | ProfileResponse;
 
+  /** Choice parameters */
   export type ChoiceParams =
     | EmptyObject
     | StartParams
@@ -94,64 +103,79 @@ export namespace AuthFlow {
 
   export type EmptyObject = {};
 
-  // Password
+  /**
+    Password choice definitions
+  */
 
+  /** Password choice response */
   export interface PasswordResponse {
     enrollment: boolean;
     password_policy: PasswordPolicy;
     need_email?: boolean;
   }
 
+  /** Password choice result */
   export interface PasswordResult {
     choice: Choice.PASSWORD;
     data: PasswordResponse;
   }
 
+  /** Set password response */
   export interface SetPasswordResponse {
     password_policy: PasswordPolicy;
   }
 
+  /** Set password result */
   export interface SetPasswordResult {
     choice: Choice.SET_PASSWORD;
     data: SetPasswordResponse;
   }
 
+  /** Reset password response */
   export interface ResetPasswordResponse {
     sent: boolean;
     resend_time: string;
     state: string;
   }
 
+  /** Reeset password result */
   export interface ResetPasswordResult {
     choice: Choice.RESET_PASSWORD;
     data: ResetPasswordResponse;
   }
 
+  /** Password parameters */
   export interface PasswordParams {
     password: string;
     email?: string;
   }
 
+  /** Password verify request */
   export interface PasswordRequest {
     flow_id: string;
     choice: Choice.PASSWORD;
     data: PasswordParams;
   }
 
+  /** Set password request */
   export interface SetPasswordRequest {
     flow_id: string;
     choice: Choice.SET_PASSWORD;
     data: PasswordParams;
   }
 
+  /** Reset password request */
   export interface ResetPasswordRequest {
     flow_id: string;
     choice: Choice.RESET_PASSWORD;
     data: EmptyObject;
   }
 
-  // Social
+  /**
+    Social choice definitions
+  */
 
+  /** Social choice response */
   export interface SocialResponse {
     enrollment: boolean;
     social_provider: string;
@@ -159,24 +183,30 @@ export namespace AuthFlow {
     redirect_uri: string;
   }
 
+  /** Social choice result */
   export interface SocialResult {
     choice: Choice.SOCIAL;
     data: SocialResponse;
   }
 
+  /** Social choice parameters */
   export interface SocialParams {
     social_provider: string;
     uri: string;
   }
 
+  /** Social choice request */
   export interface SocialRequest {
     flow_id: string;
     choice: Choice.SOCIAL;
     data: SocialParams;
   }
 
-  // SAML
+  /**
+    SAML choice definitions
+  */
 
+  /** SAML choice response */
   export interface SamlResponse {
     enrollment: boolean;
     provider_name: string;
@@ -186,11 +216,13 @@ export namespace AuthFlow {
     idp_init_flow?: boolean;
   }
 
+  /** SAML choice result */
   export interface SamlResult {
     choice: Choice.SAML;
     data: SamlResponse;
   }
 
+  /** SAML choice data */
   export interface SamlParams {
     provider_id: string;
     provider_name: string;
@@ -198,18 +230,23 @@ export namespace AuthFlow {
     idp_flow_state?: string;
   }
 
+  /** SAML request parameters */
   export interface SamlRequest {
     flow_id: string;
     choice: Choice.SAML;
     data: SamlParams;
   }
 
-  // Passkey
+  /**
+    Passkey choice definitions
+  */
 
+  /** Passkey choice data */
   export interface PasskeyData {
     publicKey: any;
   }
 
+  /** Passkey choice response */
   export interface PasskeyResponse {
     enrollment: boolean;
     started: boolean;
@@ -218,89 +255,113 @@ export namespace AuthFlow {
     registration?: PasskeyData;
   }
 
+  /** Passkey choice result */
   export interface PasskeyResult {
     choice: Choice.PASSKEY;
     data: PasskeyResponse;
   }
 
+  /** Passkey request parameters */
   export interface PasskeyRequest {
     flow_id: string;
     choice: Choice.PASSKEY;
     data: EmptyObject;
   }
 
-  // Set Email
+  /**
+    Set Email choice definitions
+  */
 
+  /** Set Email choice response */
   export interface SetEmailResponse {
     required_for: string[];
   }
 
+  /** Set Email choice result */
   export interface SetEmailResult {
     choice: Choice.SET_EMAIL;
     data: SetEmailResponse;
   }
 
+  /** Set Email choice data */
   export interface EmailParams {
     email: string;
   }
 
+  /** Set Email request parameters */
   export interface SetEmailRequest {
     flow_id: string;
     choice: Choice.SET_EMAIL;
     data: EmailParams;
   }
 
-  // Set Username
+  /**
+    Set Username choice definitions
+  */
 
+  /** Set Username choice response */
   export interface SetUsernameResponse {
     required_for: string[];
   }
 
+  /** Set Username choice result */
   export interface SetUsernameResult {
     choice: Choice.SET_USERNAME;
     data: SetUsernameResponse;
   }
 
+  /** Set Username choice data */
   export interface UsernameParams {
     username: string;
   }
 
+  /** Set Username request parameters */
   export interface SetUsernameRequest {
     flow_id: string;
     choice: Choice.SET_USERNAME;
     data: UsernameParams;
   }
 
-  // Set Phone
+  /**
+    Set Phone choice definitions
+  */
 
+  /** Set Phone choice result */
   export interface SetPhoneResult {
     choice: Choice.SET_PHONE;
     data: EmptyObject;
   }
 
+  /** Set Phone choice data */
   export interface PhoneParams {
     phone: string;
   }
 
+  /** Set Phone request parameters */
   export interface SetPhoneRequest {
     flow_id: string;
     choice: Choice.SET_PHONE;
     data: PhoneParams;
   }
 
-  // Verify Email
+  /**
+    Verify Email choice definitions
+  */
 
+  /**  Verify Email choice result */
   export interface VerifyEmailResult {
     choice: Choice.VERIFY_EMAIL;
     data: VerifyEmailResponse;
   }
 
+  /**  Verify Email chocie request parameters */
   export interface VerifyEmailRequest {
     flow_id: string;
     choice: Choice.VERIFY_EMAIL;
     data: EmptyObject;
   }
 
+  /**  Verify Email choice response */
   export interface VerifyEmailResponse {
     sent: boolean;
     resend_time?: string;
@@ -308,38 +369,49 @@ export namespace AuthFlow {
     need_email?: boolean;
   }
 
+  /**  Verify Email chocie result */
   export interface ResultVerifyEmail {
     choice: Choice.VERIFY_EMAIL;
     data: VerifyEmailResponse;
   }
 
+  /**  Verify Email restart parameters */
   export interface VerifyEmailRestart {
     email?: string;
   }
 
-  // Captcha
+  /**
+    Captcha choice definitions
+  */
 
+  /** Captcha choice response */
   export interface CaptchaResponse {
     site_key: string;
   }
 
+  /** Captcha choice result */
   export interface CaptchaResult {
     choice: Choice.CAPTCHA;
     data: CaptchaResponse;
   }
 
+  /** Captcha choice data */
   export interface CaptchaParams {
     code: string;
   }
 
+  /** Captcha request parameters */
   export interface CaptchaRequest {
     flow_id: string;
     choice: Choice.CAPTCHA;
     data: CaptchaParams;
   }
 
-  // Email OTP
+  /**
+    Email OTP choice definitions
+  */
 
+  /** Email OTP choice response */
   export interface EmailOtpResponse {
     sent: boolean;
     enrollment: boolean;
@@ -347,27 +419,34 @@ export namespace AuthFlow {
     need_email?: boolean;
   }
 
+  /** Email OTP choice result */
   export interface EmailOtpResult {
     choice: Choice.EMAIL_OTP;
     data: EmailOtpResponse;
   }
 
+  /** Email OTP choice data */
   export interface EmailOtpParams {
     code: string;
   }
 
+  /** Email OTP request parameters */
   export interface EmailOtpRequest {
     flow_id: string;
     choice: Choice.EMAIL_OTP;
     data: EmailOtpParams;
   }
 
+  /** Email OTP restart parameters */
   export interface EmailOtpRestart {
     email: string;
   }
 
-  // SMS OTP
+  /**
+    SMS OTP choice definitions
+  */
 
+  /** SMS OTP choice response */
   export interface SmsOtpResponse {
     sent: boolean;
     enrollment: boolean;
@@ -375,59 +454,74 @@ export namespace AuthFlow {
     need_phone?: string;
   }
 
+  /** SMS OTP choice result */
   export interface SmsOtpResult {
     choice: Choice.SMS_OTP;
     data: SmsOtpResponse;
   }
 
+  /** SMS OTP choice data */
   export interface SmsOtpParams {
     code: string;
   }
 
+  /** SMS OTP request parameters */
   export interface SmsOtpRequest {
     flow_id: string;
     choice: Choice.SMS_OTP;
     data: SmsOtpParams;
   }
 
+  /** SMS OTP Restart parameters */
   export interface SmsOtpRestart {
     phone: string;
   }
 
-  // TOTP
+  /**
+    TOTP choice definitions
+  */
 
+  /** TOTP QR Code parameters */
   export interface TotpSecret {
     qr_image: string;
     secret: string;
   }
 
+  /** TOTP choice response */
   export interface TotpResponse {
     enrollment: string;
     totp_secret?: TotpSecret;
   }
 
+  /** TOTP choice result */
   export interface TotpResult {
     choice: Choice.TOTP;
     data: TotpResponse;
   }
 
+  /** TOTP choice data */
   export interface TotpParams {
     code: string;
   }
 
+  /** TOTP request parameters */
   export interface TotpRequest {
     flow_id: string;
     choice: Choice.TOTP;
     data: TotpParams;
   }
 
-  // Magiclink
+  /**
+    Magiclink choice definitions
+  */
 
+  /** Magiclink choice result */
   export interface MagiclinkResult {
     choice: Choice.MAGICLINK;
     data: MagiclinkResponse;
   }
 
+  /** Magiclink choice response */
   export interface MagiclinkResponse {
     sent: boolean;
     resend_time?: string;
@@ -435,25 +529,33 @@ export namespace AuthFlow {
     need_email?: boolean;
   }
 
+  /** Magiclink restart parameters */
   export interface MagiclinkRestart {
     email: string;
   }
 
-  // Provisional
+  /**
+    Provisional choice definitions
+  */
 
+  /** Provisional choice result */
   export interface ProvisionalResult {
     choice: Choice.PROVISIONAL;
     data: ProvisionalResponse;
   }
 
+  /** Provisional choice response */
   export interface ProvisionalResponse {
     sent: boolean;
     resend_time?: string;
     state: string;
   }
 
-  // Agreements
+  /**
+    Agreements choice definitions
+  */
 
+  /** Agreeement definition */
   export interface AgreementData {
     id: string;
     type: string;
@@ -464,53 +566,67 @@ export namespace AuthFlow {
     text: string;
   }
 
+  /** Agreement choice response */
   export interface AgreementsResponse {
     agreements: { [key: string]: AgreementData };
   }
 
+  /** Agreement choice result */
   export interface AgreementsResult {
     choice: Choice.AGREEMENTS;
     data: AgreementsResponse;
   }
 
+  /** Agreement parameter list */
   export interface AgreementsParams {
     agreed: string[];
   }
 
+  /** Agreement choice request parameters */
   export interface AgreementsRequest {
     flow_id: string;
     choice: Choice.AGREEMENTS;
     data: AgreementsParams;
   }
 
-  // User Consent
+  /**
+    User Consent choice definitions - for OAuth
+  */
 
+  /** Scope parameters */
   export interface SelectedScope {
     scope: string;
     is_allowed: boolean;
   }
 
+  /** Consent choice response */
   export interface ConsentResponse {
     scopes: Scope[];
   }
 
+  /** Consent choice result */
   export interface ConsentResult {
     choice: Choice.CONSENT;
     data: ConsentResponse;
   }
 
+  /** Selected scope parameter list */
   export interface ConsentParams {
     scope_selections: SelectedScope[];
   }
 
+  /** Consent request parameters */
   export interface ConsentRequest {
     flow_id: string;
     choice: Choice.CONSENT;
     data: ConsentParams;
   }
 
-  // Profile
+  /**
+    Profile choice definitions
+  */
 
+  /** Profile field parameters */
   export interface ProfileField {
     id: string;
     label: string;
@@ -520,49 +636,58 @@ export namespace AuthFlow {
     show_on_signup: boolean;
   }
 
+  /** Profile choice response */
   export interface ProfileResponse {
     fields: ProfileField[];
   }
 
+  /** Profile choice result */
   export interface ProfileResult {
     choice: Choice.PROFILE;
     data: ProfileResponse;
   }
 
+  /** Profile field list parameters */
   export interface ProfileParams {
     profile: { [key: string]: string };
   }
 
+  /** Profile request parameters */
   export interface ProfileRequest {
     flow_id: string;
     choice: Choice.PROFILE;
     data: ProfileParams;
   }
 
-  // Status
+  /**
+    Misc definitions
+  */
 
+  /** Status choice request */
   export interface StatusRequest {
     flow_id: string;
     choice: Choice.NONE;
     data: EmptyObject;
   }
 
-  // Conditional MFA
+  /** Conditional MFA  parameters */
   export interface ConditionalMfaConfig {
     strict_mode: boolean;
     lifetime: number;
   }
 
-  // Scope
-
+  /** OAuth Scope definition */
   export interface Scope {
     name: string;
     display_name: string;
     description: string;
   }
 
-  // Result, Request and State types
+  /**
+    Result, Request and State types
+  */
 
+  /** Result definition */
   export type Result =
     | SetEmailResult
     | SetUsernameResult
@@ -584,6 +709,7 @@ export namespace AuthFlow {
     | ProvisionalResult
     | ConsentResult;
 
+  /** State data definition */
   export interface StateData {
     flowId: string;
     flowType: string[];
@@ -622,6 +748,7 @@ export namespace AuthFlow {
     scopes?: Scope[];
   }
 
+  /** Start request parameters */
   export interface StartRequest {
     cb_uri: string;
     flow_types: string[];
@@ -631,6 +758,7 @@ export namespace AuthFlow {
     client_id?: string;
   }
 
+  /** Update request options */
   export type UpdateRequest =
     | SetEmailRequest
     | SetPhoneRequest
@@ -651,17 +779,20 @@ export namespace AuthFlow {
     | ProfileRequest
     | ConsentRequest;
 
+  /** Base flow request parameters */
   export interface BaseRequest {
     flow_id: string;
     choice: "";
     data: {};
   }
 
+  /** Complete request parameters */
   export interface CompleteRequest {
     flow_id: string;
     device_id?: string;
   }
 
+  /** Restart choice options */
   export type RestartChoice =
     | Choice.EMAIL_OTP
     | Choice.SMS_OTP
@@ -672,6 +803,7 @@ export namespace AuthFlow {
     | Choice.MAGICLINK
     | Choice.PROVISIONAL;
 
+  /** Restart choice parameters */
   export interface RestartRequest {
     flow_id: string;
     choice: RestartChoice;
--- packages/vanilla-js/src/types.ts
+++ packages/vanilla-js/src/types.ts
@@ -1,15 +1,24 @@
+/**
+ * Describes the content of a API response for a Pangea request
+ */
 export interface APIResponse {
   status: string;
   summary: string;
   result: any;
 }
 
+/**
+ * Describes the client configuration properties
+ */
 export interface ClientConfig {
   domain: string;
   clientToken: string;
   callbackUri?: string;
 }
 
+/**
+ * Describes the body of a response object for a Pangea request
+ */
 export interface ClientResponse {
   success: boolean;
   response: APIResponse;
