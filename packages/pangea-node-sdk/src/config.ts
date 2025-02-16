0b065944dba1398c9792e20ddfa9d9cbca892b13
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-02-16 12:50:34 -0500

    pangea-node-sdk: publish v4.4.0

--- packages/pangea-node-sdk/src/config.ts
+++ packages/pangea-node-sdk/src/config.ts
@@ -1,6 +1,6 @@
 import { ConfigOptions, ConfigEnv } from "./types.js";
 
-export const version = "4.3.0";
+export const version = "4.4.0";
 
 /** Configuration for a Pangea service client. */
 class PangeaConfig {
