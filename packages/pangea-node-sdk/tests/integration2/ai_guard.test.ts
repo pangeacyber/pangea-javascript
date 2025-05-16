cc3cb931713d8ea608dc1b28e0f96b0f02c6be0d
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-05-16 15:59:10 -0400

    pangea-node-sdk: add mock integration tests

---
+++ packages/pangea-node-sdk/tests/integration2/ai_guard.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, it } from "vitest";
+
+import PangeaConfig from "../../src/config.js";
+import AIGuardService from "../../src/services/ai_guard.js";
+
+const client = new AIGuardService(
+  "my_api_token",
+  new PangeaConfig({
+    baseUrlTemplate: process.env.TEST_API_BASE_URL ?? "http://127.0.0.1:4010",
+  })
+);
+
+describe("AI Guard", () => {
+  it("guardText: text", async () => {
+    const response = await client.guardText({
+      text: "what was pangea?",
+      recipe: "my_recipe",
+      debug: true,
+      log_fields: {
+        source: "foobar",
+      },
+    });
+    expect(response.status).toStrictEqual("Success");
+    expect(response.result).toBeDefined();
+  });
+
+  it("guardText: messages", async () => {
+    const response = await client.guardText({
+      text: "what was pangea?",
+      recipe: "my_recipe",
+      debug: true,
+      log_fields: {
+        source: "foobar",
+      },
+    });
+    expect(response.status).toStrictEqual("Success");
+    expect(response.result).toBeDefined();
+  });
+});
