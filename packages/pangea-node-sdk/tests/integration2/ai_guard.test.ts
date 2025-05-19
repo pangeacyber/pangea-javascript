b9cf99af69ce4657ffdabfd3666ae3a06dfd3e8c
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-05-19 12:34:01 -0400

    pangea-node-sdk: add AI Guard config APIs

--- packages/pangea-node-sdk/tests/integration2/ai_guard.test.ts
+++ packages/pangea-node-sdk/tests/integration2/ai_guard.test.ts
@@ -36,4 +36,85 @@ describe("AI Guard", () => {
     expect(response.status).toStrictEqual("Success");
     expect(response.result).toBeDefined();
   });
+
+  it("getServiceConfig", async () => {
+    const response = await client.getServiceConfig("my_service_config");
+    expect(response.status).toStrictEqual("Success");
+    expect(response.result).toBeDefined();
+    expect(response.result.audit_data_activity).toBeTypeOf("object");
+    expect(response.result.connections).toBeTypeOf("object");
+    expect(response.result.id).toBeTypeOf("string");
+    expect(response.result.name).toBeTypeOf("string");
+    expect(response.result.recipes).toBeTypeOf("object");
+  });
+
+  it("createServiceConfig", async () => {
+    const response = await client.createServiceConfig({
+      name: "my_service_config",
+      audit_data_activity: {
+        enabled: true,
+        audit_service_config_id: "my_audit_service_config",
+        areas: {
+          text_guard: true,
+        },
+      },
+    });
+    expect(response.status).toStrictEqual("Success");
+    expect(response.result).toBeDefined();
+    expect(response.result.audit_data_activity).toBeTypeOf("object");
+    expect(response.result.connections).toBeTypeOf("object");
+    expect(response.result.id).toBeTypeOf("string");
+    expect(response.result.name).toBeTypeOf("string");
+    expect(response.result.recipes).toBeTypeOf("object");
+  });
+
+  it("updateServiceConfig", async () => {
+    const response = await client.updateServiceConfig({
+      id: "my_service_config",
+      name: "my_service_config",
+      audit_data_activity: {
+        enabled: true,
+        audit_service_config_id: "my_audit_service_config",
+        areas: {
+          text_guard: true,
+        },
+      },
+    });
+    expect(response.status).toStrictEqual("Success");
+    expect(response.result).toBeDefined();
+    expect(response.result.audit_data_activity).toBeTypeOf("object");
+    expect(response.result.connections).toBeTypeOf("object");
+    expect(response.result.id).toBeTypeOf("string");
+    expect(response.result.name).toBeTypeOf("string");
+    expect(response.result.recipes).toBeTypeOf("object");
+  });
+
+  it("deleteServiceConfig", async () => {
+    const response = await client.deleteServiceConfig("my_service_config");
+    expect(response.status).toStrictEqual("Success");
+    expect(response.result).toBeDefined();
+    expect(response.result.audit_data_activity).toBeTypeOf("object");
+    expect(response.result.connections).toBeTypeOf("object");
+    expect(response.result.id).toBeTypeOf("string");
+    expect(response.result.name).toBeTypeOf("string");
+    expect(response.result.recipes).toBeTypeOf("object");
+  });
+
+  it("listServiceConfigs", async () => {
+    const response = await client.listServiceConfigs({
+      filter: {
+        id: "foo",
+        id__contains: ["foo", "bar"],
+      },
+      last: "foo",
+      order: "asc",
+      order_by: "id",
+      size: 10,
+    });
+    expect(response.status).toStrictEqual("Success");
+    expect(response.result).toBeDefined();
+    expect(response.result.count).toBeTypeOf("number");
+    expect(response.result.last).toBeTypeOf("string");
+    expect(response.result.items).toBeDefined();
+  });
 });
